7776888f32fa4402606dc489dcc23a03fd69abbc
Akil Mullassery Prashant <94084424+akilmp@users.noreply.github.com>
2025-08-03 04:35:18 +1000

    feat: add networking resources

--- terraform/core-network/outputs.tf
+++ terraform/core-network/outputs.tf
@@ -9,3 +9,23 @@ output "public_subnet_ids" {
 output "private_subnet_ids" {
   value = aws_subnet.private[*].id
 }
+
+output "internet_gateway_id" {
+  value = aws_internet_gateway.this.id
+}
+
+output "nat_gateway_id" {
+  value = aws_nat_gateway.this.id
+}
+
+output "public_route_table_id" {
+  value = aws_route_table.public.id
+}
+
+output "private_route_table_id" {
+  value = aws_route_table.private.id
+}
+
+output "security_group_id" {
+  value = aws_security_group.default.id
+}
--- terraform/core-network/variables.tf
+++ terraform/core-network/variables.tf
@@ -27,3 +27,15 @@ variable "availability_zones" {
   type        = list(string)
   description = "Availability zones for subnets"
 }
+
+variable "nat_gateway_subnet_index" {
+  type        = number
+  description = "Index of the public subnet in which to place the NAT Gateway"
+  default     = 0
+}
+
+variable "security_group_ingress_cidr_blocks" {
+  type        = list(string)
+  description = "CIDR blocks allowed to access the default security group"
+  default     = ["0.0.0.0/0"]
+}
