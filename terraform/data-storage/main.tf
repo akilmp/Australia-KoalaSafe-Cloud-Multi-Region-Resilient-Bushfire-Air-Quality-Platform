2bd6ed446c08c6d22ebf3ccf175146be998860b0
Akil Mullassery Prashant <94084424+akilmp@users.noreply.github.com>
2025-08-03 05:03:56 +1000

    Add default SSE encryption to data buckets

--- terraform/data-storage/main.tf
+++ terraform/data-storage/main.tf
@@ -15,6 +15,14 @@ resource "aws_s3_bucket" "data" {
     enabled = true
   }
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
   lifecycle_rule {
     id      = "transition-ia"
     enabled = true
@@ -33,6 +41,14 @@ resource "aws_s3_bucket" "data_replica" {
 
   versioning {
     enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
   }
 }
 
